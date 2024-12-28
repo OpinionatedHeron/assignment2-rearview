78bfcec7ae1e962c1b381e60d8c33bd9f07fd018
Leanne <leamoy@gmail.com>
2024-12-28 05:00:25 +0000

    Trying Blynk - might need to switch

--- rearview_camera.py
+++ rearview_camera.py
@@ -1,4 +1,5 @@
 import BlynkLib
+import time
 import cv2
 from sense_hat import SenseHat
 from picamera2 import Picamera2
@@ -6,9 +7,26 @@ from flask import Flask, Response
 
 #initialise SenseHAT
 sense = SenseHat()
+sense.clear()
 
 #Blynk authentication token
 BLYNK_AUTH = 'wCsvZULPg7-9L5mKwR9ZOEfwA_aV7qV3'
 
 #Initialise Blynk instance
 blynk = BlynkLib.Blynk(BLYNK_AUTH)
+
+REVERSE = -1.0 #Setting backward movement detection
+FORWARD = 1.0 #Setting forward movement detection
+
+#Tracks if the camera is recording or not
+is_recording = FALSE
+
+@blynk.on("V1")
+try:
+    while True:
+            #get acceleration reading
+            acceleration = sense.get_accelerometer_raw()
+            z_axis = acceleration['z'] #z axiz is when RPi is flat, not rotated
+
+            if z_axis < REVERSE:
+                    camera.start_recording('motion_video.h264')
